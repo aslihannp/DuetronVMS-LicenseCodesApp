4791fc4ee4f2d418725fb6ceaf351e2354e1c599
Aslıhan Nur Pehlivan <66755756+aslihannp@users.noreply.github.com>
2020-11-07 18:13:58 +0300

    Add files via upload

---
+++ Entities/ClientTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMS.Entities
+{
+    public class ClientTable
+    {
+        public int UC;
+        public string ClientName;
+    }
+}
